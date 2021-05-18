76272176bb57343f9ddf7a76755d093b8d0964df
roum02 <78200124+roum02@users.noreply.github.com>
2021-05-18 23:40:31 +0900

    Add files via upload

--- JS_study/first.js
+++ JS_study/first.js
@@ -1,10 +1,11 @@
-console.log(sum(1,2));
-console.log(multiply(3,4));
-
-function sum(a,b) {     //함수선언문
-  return a + b;
-}
-
-var multiply = function(a,b){   //함수표현식
-  return a * b
-}
+var a = 1;
+var outer = function(){
+  var inner = function(){
+    console.log(a);
+    var a = 3;
+  };
+  inner();
+  console.log(a);
+};
+outer();
+console.log(a);
