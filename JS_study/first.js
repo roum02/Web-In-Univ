501b357907a5ca2abbecbccc54144d1b6d672a14
roum02 <78200124+roum02@users.noreply.github.com>
2021-06-22 23:37:03 +0900

    Add files via upload

--- JS_study/first.js
+++ JS_study/first.js
@@ -1,9 +1,26 @@
-var func = function(x){
-  console.log(this, x);
+//전통적인 방식
+var obj1 = {
+  name:'obj1',
+  func: function(){
+    var self = this;
+    return function(){
+      console.log(self.name);
+    };
+  }
 };
-func(1);
 
-var obj = {
-  method: func
+var callback = obj.func();
+setTimeout(callback, 1000);
+
+//함수를 다른 객체에 재활용하는 상황
+var obj2 = {
+  name:'obj2',
+  func:obj1.func
 };
-obj.method(2);
+
+var callback2 = obj2.func();
+setTimeout(callback2, 1500);
+
+var obj3 = {name:'obj3'};
+var callback3 = obj1.func.call(obj3);
+setTimeout(callback3, 2000);
