aad916d215b6f3032c93f9a7cf298eee9af3aa55
roum02 <78200124+roum02@users.noreply.github.com>
2021-06-24 23:44:12 +0900

    Add files via upload

--- JS_study/first.js
+++ JS_study/first.js
@@ -1,26 +1,14 @@
-//전통적인 방식
-var obj1 = {
-  name:'obj1',
-  func: function(){
-    var self = this;
-    return function(){
-      console.log(self.name);
-    };
-  }
-};
+//콜백함수와 클로저(1)
+var fruits = ['apple','banana','peach'];
+var $ul = document.createElement('ul');
 
-var callback = obj.func();
-setTimeout(callback, 1000);
+fruits.forEach(function(fruits){
+  var $li = document.createElement('li');
+  $li.innerText = fruit;
+  $li.addEventListener('click', function(){
+    alert('your choice is' + fruit);
+  });
+  $ul.appendChild($li);
+});
 
-//함수를 다른 객체에 재활용하는 상황
-var obj2 = {
-  name:'obj2',
-  func:obj1.func
-};
-
-var callback2 = obj2.func();
-setTimeout(callback2, 1500);
-
-var obj3 = {name:'obj3'};
-var callback3 = obj1.func.call(obj3);
-setTimeout(callback3, 2000);
+document.body.appendChild($ul);
